b67498ce177fb64cf5f6ba496e8457c3b0d9f6b5
ivorguzman <ivorguzman7@gmail.com>
2023-08-04 17:52:19 -0400

    04082023

--- 74-76__PaginacionDeDatos/74_76_index.php
+++ 74-76__PaginacionDeDatos/74_76_index.php
@@ -20,6 +20,14 @@
       "74-76_conexion.php". */
       include("74-76_conexion.php");
 
+      /* La línea ` = new PDO(, , );` está creando un nuevo objeto
+      PDO y estableciendo una conexión con la base de datos. La variable ` $conexion_pdo ` contiene el DNS o
+      DSN (Nombre de la fuente de datos) de la base de datos, que especifica el controlador de la
+      base de datos, el host y el nombre de la base de datos. Las variables ` $usuario` y ` $password`
+      contienen el nombre de usuario y la contraseña para la conexión a la base de datos. La función
+      `nuevo PDO()` crea una nueva instancia de la clase PDO, que es una extensión de PHP para
+      interactuar con bases de datos. El objeto  resultante se puede usar para ejecutar
+       consultas SQL y realizar otras operaciones de base de datos. */
       $conexion_pdo = new PDO($dns, $usuario, $password);
       echo "_21 Conexion establecida con BASE DE DATOS (21 index.php) " . "<br>";
 
@@ -30,17 +38,44 @@
       /* La línea `->exec("SET CHARACTER SET utf8");` está configurando el conjunto de   caracteres de la conexión de la base de datos a UTF-8. Esto asegura que la conexión pueda manejar   y almacenar caracteres de diferentes idiomas y conjuntos de caracteres correctamente. UTF-8 es   una codificación de caracteres ampliamente utilizada que admite una amplia gama de caracteres de   varios idiomas. */
       $conexion_pdo->exec("SET CHARACTER SET utf8");
 
+      /* Este código verifica si el parámetro "página" está configurado en la URL. Si está configurado, 
+        comprueba si el valor del parámetro "pagina" es igual a 1. Si es igual a 1, 
+        redirige al usuario a   la página "74_76_index.php". Si el valor del parámetro "pagina"
+         no es igual a 1, asigna el valor   del parámetro "pagina" a la variable . */
+      if (isset($_GET["pagina"])) {
 
-      $Pagina = 1; // Mostrar pagia donde estamos al cargar por primera vez la Pagia web
+         if ($_GET['pagina'] == 1) {
+            header("Location:74_76_index.php");
+         } else {
+            $pagina = $_GET["pagina"];
+
+            // ===COMPROBACIONES===
+            print "<pre>\n";
+            print_r("Pagina  = " . $pagina . "<br>");
+            print "<pre>";
+            // ===FIN COMPROBACIONES
+
+         }
+      } else {
+         $pagina = 1;
+         // ===COMPROBACIONES===
+         print "<pre>\n";
+         print_r("pagina  = " . $pagina . "<br>");
+         print "<pre>";
+         // ===FIN COMPROBACIONES
+
+      }
+
+      $pagina = 1; // Mostrar pagia donde estamos al cargar por primera vez la Pagia web
       $tamhnoPagina = 3; // Cuato registros ver por Pagina
 
       /* La línea ` =(-1) * ;` está calculando el índice de inicio para
      los registros que se mostrarán en la página actual. */
-      $empezarDesDe = ($Pagina - 1) * $tamhnoPagina;
+      $empezarDesDe = ($pagina - 1) * $tamhnoPagina;
 
 
-      // $sql = ("SELECT  * FROM  productos ");
-      $sql = ("SELECT  * FROM  productos WHERE SECCION= 'DEPORTES'");
+      $sql = ("SELECT  * FROM  productos ");
+      // $sql = ("SELECT  * FROM  productos WHERE SECCION= 'DEPORTES'");
 
       /* Se está asignando el resultado de la ejecución de la sentencia
       preparada . La sentencia preparada se ejecuta utilizando el método
@@ -73,15 +108,16 @@
       // print_r("$registro == $.conexion_pdo->query("SELECT * FROM productos")->fetchAll(PDO::FETCH_OBJ));
       print_r("Numero de fila (Registros) en la consulta = " . $numeroFila . "<br>");
       print_r("Numero de fila (Registros) por pagina = " . $tamhnoPagina . "<br>");
-      print_r("Numero de Paginas en la consulta = "  .  $Pagina . " de" . " $totalPaginas" . "<br>");
-      print_r("Empezar Desde = " . $empezarDesDe );
-      print_r(" Hasta = " . $tamhnoPagina ."<br>");
+      print_r("Numero de Paginas en la consulta = "  .  $pagina . " de" . " $totalPaginas" . "<br>");
+      print_r("Empezar Desde = " . $empezarDesDe);
+      print_r(" Hasta = " . $tamhnoPagina . "<br>");
       print "<pre>";
       // ===FIN COMPROBACIONES
 
       /* La línea de código `  = ("SELECT * FROM productos LIMIT , ");`   está creando una consulta SQL
        para seleccionar un número limitado de filas de la tabla   "productos". La cláusula `LIMIT` 
        se utiliza para limitar el número de filas devueltas por la   consulta. */
+      // $sqlLimit = ("SELECT  * FROM  productos");
       $sqlLimit = ("SELECT  * FROM  productos LIMIT  $empezarDesDe, $tamhnoPagina");
 
       /* Se está asignando el resultado de la ejecución de la sentencia
@@ -113,14 +149,12 @@
       echo "<br>";
       echo "<br>";
       echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "|____________PAGINACIÓN_____________| " . "<br>";
+      //|____________PAGINACIÓN_____________| "
 
-
-
-
-
+      for ($i = 1; $i < $totalPaginas; $i++) {
+         echo "<a href='?pagina=" . $i . "'>|$i|</a>  ";
+      }
+      echo " Siguiente";
    } catch (Throwable $e) {
       echo "_ ALEX, ERROR: el codigo de execpción es: " . $e->getMessage() . "<br />";
       echo "_ EL archivo es: " . $e->getFile() . "<br />";
